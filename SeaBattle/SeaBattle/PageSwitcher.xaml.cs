0312a0e702937d6e982d497ad65824cca854645d
Vitalii_Korunov <v.korunov@mail.ru>
2016-09-03 15:14:39 +0400

    no message

---
+++ SeaBattle/SeaBattle/PageSwitcher.xaml.cs
@@ -0,0 +1,36 @@
+﻿using SeaBattle.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SeaBattle
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class PageSwitcher : Window
+    {
+        public PageSwitcher()
+        {
+            InitializeComponent();
+            Switcher.pageSwitcher = this;
+            Switcher.SwitchPage(new Login()); 
+        }
+
+        public void Navigate(UserControl nextPage)
+        {
+            this.Content = nextPage;
+        }
+    }
+}
---
+++ SeaBattle/SeaBattle/Switcher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SeaBattle
+{
+    public static class Switcher
+    {
+        public static PageSwitcher pageSwitcher;
+
+        public static void SwitchPage(UserControl newPage)
+        {
+            pageSwitcher.Navigate(newPage);
+        }
+    }
+}
