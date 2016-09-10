c06b61a2f9b97f3a40534f25fbac4d58b4e135cf
Vitalii_Korunov <v.korunov@mail.ru>
2016-09-10 22:28:13 +0400

    Добавил файл конфигурации, где будет настройки ограничения пароля и логина

---
+++ SeaBattle/SeaBattle/ConfigSections/SettingsValidDataConfigSections.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace SeaBattle.ConfigSections
+{
+    public class SettingsValidDataConfigSections : ConfigurationSection
+    {
+        [ConfigurationProperty("minLengthUsername")]
+        public int MinLengthUsername
+        {
+            get
+            {
+                var str = (base["minLengthUsername"]).ToString();
+                return int.Parse(str);
+            }
+        }
+
+        [ConfigurationProperty("minLengthPassword")]
+        public int MinLengthPassword
+        {
+            get
+            {
+                var str = (base["minLengthPassword"]).ToString();
+                return int.Parse(str);
+            }
+        }
+
+        [ConfigurationProperty("maxLengthPassword")]
+        public int MaxLengthPassword
+        {
+            get
+            {
+                var str = (base["maxLengthPassword"]).ToString();
+                return int.Parse(str);
+            }
+        }
+    }
+}
